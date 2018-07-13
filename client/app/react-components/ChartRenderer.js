2b55ff03d27df6a3d2f3b1b5c7a09b31acd0e99c
Allen Short <ashort@mozilla.com>
2018-07-13 14:52:10 -0500

    misc wip

--- client/app/react-components/ChartRenderer.js
+++ client/app/react-components/ChartRenderer.js
@@ -9,11 +9,12 @@ export default class ChartRenderer extends React.PureComponent {
     // eslint-disable-next-line react/no-unused-prop-types
     queryResult: PropTypes.object.isRequired,
     options: PropTypes.object.isRequired,
+    filters: PropTypes.array.isRequired,
   }
 
   render() {
     let chartSeries;
-    if (!isUndefined(this.props.queryResult) && this.props.queryResult.getData()) {
+    if (!isUndefined(this.props.queryResult) && this.props.queryResult.getData(this.props.filters)) {
       const data = this.props.queryResult.getChartData(this.props.options.columnMapping);
       chartSeries = sortBy(data, (o, s) => get(o.seriesOptions, [s && s.name, 'zIndex'], 0));
     } else {
